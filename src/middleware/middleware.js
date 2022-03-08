d7ca12284dacf3ca269f6d7f400eac1540ce6b42
Arati <aratichaudhari100@gmail.com>
2022-03-08 09:55:56 +0530

    done

--- src/middleware/middleware.js
+++
@@ -1,8 +0,0 @@
-const isFreeAppUser=function(req,res,next){
-    let data=req.headers["isFreeAppUser"]
-    if(data!==undefined){
-        res.send("request is missing")
-    }
-    next()
-}
-module.exports.isFreeAppUser=isFreeAppUser
